b1c950bdffbc91c8ec7185ab606d4789ee83ee1a
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-03 12:09:12 -0500

    Add navbar and singles page to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
@@ -9,13 +10,21 @@ import {
 export class App extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          {/*Aca deben ir los links de navegacion*/}
-        </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
-      </div>
-    )
+      <Router>
+        <div>
+          <nav>
+            <Link to="/page1">Page 1</Link>
+            <Link to="/page2">Page 2</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Page1} />
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
